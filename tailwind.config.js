5973f782d9bb0f0ce291a01fd4bc3cd1bc9b5ba1
Erick Koga <pessoal@erickkoga.com>
2024-08-01 01:49:33 -0300

    feat: add custom font, color palette, and Tailwind rules sorter
    - Import custom font
    - Configure Tailwind with custom colors and font family
    - Add .prettierrc with prettier-plugin-tailwindcss for Tailwind class sorting

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,43 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['DIN Pro', 'sans-serif'],
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        constructive: {
+          DEFAULT: 'hsl(var(--constructive))',
+          foreground: 'hsl(var(--constructive-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        border: 'hsl(var(--border))'
+      },
+    },
+    fontSize: {
+      'sm': '12px',
+      'md': '16px',
+      'lg': '24px',
+      'xl': '42px'
+    }
+  },
   plugins: [],
 }
